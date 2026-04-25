e3e9f5e8833372c9d02df343de0152adee2b608a
luckyluka <lu.kavcic@gmail.com>
2026-04-25 21:30:38 +0200

    feat(scanner): add language detection by file extension

--- scanner.py
+++ scanner.py
@@ -1,5 +1,18 @@
 import os
 
+EXTENSION_MAP = {
+    ".py": "python",
+    ".c": "c",
+    ".h": "c",
+    ".cpp": "cpp",
+    ".hpp": "cpp",
+    ".js": "javascript",
+    ".ts": "typescript",
+    ".go": "go",
+    ".rs": "rust",
+    ".java": "java",
+    ".sh": "shell",
+}
 
 def scan_directory(root_path):
     file_records = []
@@ -11,12 +24,17 @@ def scan_directory(root_path):
             try:
                 stat = os.stat(full_path)
                 file_records.append({
-                    "path": full_path,
-                    "size": stat.st_size,
-                    "modified_at": stat.st_mtime
-                })
+                "path": full_path,
+                "size": stat.st_size,
+                "modified_at": stat.st_mtime,
+                "language": detect_language(filename)
+            })
             except OSError:
                 # Skip files we can't access
                 continue
 
     return file_records
+
+def detect_language(filename):
+    _, ext = os.path.splitext(filename)
+    return EXTENSION_MAP.get(ext.lower(), "unknown")
